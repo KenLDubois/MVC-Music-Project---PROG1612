7bf2f3d75a58653ac732ee726d168f21000c4191
KenLDubois <40549554+KenLDubois@users.noreply.github.com>
2018-10-10 22:23:53 -0400

    Assignment-3 Complete (without bonuses)

--- kdubois1_MVC_Music/Controllers/MusiciansController.cs
+++ kdubois1_MVC_Music/Controllers/MusiciansController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using kdubois1_MVC_Music.Data;
 using kdubois1_MVC_Music.Models;
+using kdubois1_MVC_Music.ViewModels;
 
 namespace kdubois1_MVC_Music.Controllers
 {
@@ -20,10 +21,93 @@ namespace kdubois1_MVC_Music.Controllers
         }
 
         // GET: Musicians
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? InstrumentID, string SearchString, string SortDirection, string SortField, string ActionButton)
         {
-            var kdubois1_MVC_MusicContext = _context.Musicians.Include(m => m.Instrument);
-            return View(await kdubois1_MVC_MusicContext.ToListAsync());
+            PopulateInstrumentDropdown();
+
+
+            var musicians = from m in _context.Musicians
+                            .Include(i => i.Instrument)
+                            select m;
+
+            if (InstrumentID.HasValue)
+            {
+                musicians = musicians.Where(m => m.InstrumentID == InstrumentID);
+            }
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                musicians = musicians.Where(m => m.DisplayName.ToUpper().Contains(SearchString.ToUpper()));
+            }
+
+
+            if (!string.IsNullOrEmpty(ActionButton))
+            {
+                if(ActionButton != "Filter")
+                {
+                    if(ActionButton == SortField)
+                    {
+                        SortDirection = string.IsNullOrEmpty(SortDirection) ? "desc" : "";
+                    }
+
+                    SortField = ActionButton;
+                }
+            }
+
+            musicians = musicians.OrderBy(m => m.DisplayName);
+
+            if (SortField == "DisplayName")
+            {
+                if (string.IsNullOrEmpty(SortDirection))
+                {
+                    musicians = musicians.OrderBy(m => m.DisplayName);
+                }
+                else
+                {
+                    musicians = musicians.OrderByDescending(m => m.DisplayName);
+                }
+            }
+
+            if(SortField == "Age")
+            {
+                if (string.IsNullOrEmpty(SortDirection))
+                {
+                    musicians = musicians.OrderBy(m => m.Age);
+                }
+                else
+                {
+                    musicians = musicians.OrderByDescending(m => m.Age);
+                }
+            }
+
+            if(SortField == "Phone")
+            {
+                if (string.IsNullOrEmpty(SortDirection))
+                {
+                    musicians = musicians.OrderBy(m => m.Phone);
+                }
+                else
+                {
+                    musicians = musicians.OrderByDescending(m => m.Phone);
+                }
+            }
+
+            if(SortField == "Instrument")
+            {
+                if (string.IsNullOrEmpty(SortDirection))
+                {
+                    musicians = musicians.OrderBy(m => m.Instrument.Name);
+                }
+                else
+                {
+                    musicians = musicians.OrderByDescending(m => m.Instrument.Name);
+                }
+            }
+
+            ViewData["SortField"] = SortField;
+            ViewData["SortDirection"] = SortDirection;
+
+            return View(await musicians.ToListAsync());
         }
 
         // GET: Musicians/Details/5
@@ -37,6 +121,7 @@ namespace kdubois1_MVC_Music.Controllers
             var musician = await _context.Musicians
                 .Include(m => m.Instrument)
                 .FirstOrDefaultAsync(m => m.ID == id);
+
             if (musician == null)
             {
                 return NotFound();
@@ -48,8 +133,10 @@ namespace kdubois1_MVC_Music.Controllers
         // GET: Musicians/Create
         public IActionResult Create()
         {
+            var musician = new Musician();
+            musician.Plays = new List<Plays>();
             PopulateInstrumentDropdown();
-
+            PopulateAssignedInstrumentData(musician);
             return View();
         }
 
@@ -58,10 +145,21 @@ namespace kdubois1_MVC_Music.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,StageName,FName,MName,LName,Phone,DOB,SIN,InstrumentID")] Musician musician)
+        public async Task<IActionResult> Create([Bind("ID,StageName,FName,MName,LName,Phone,DOB,SIN,InstrumentID")] Musician musician, string[] selectedInstruments)
         {
             try
             {
+                if(selectedInstruments != null)
+                {
+                    musician.Plays = new List<Plays>();
+                    foreach(var inst in selectedInstruments)
+                    {
+                        var instToAdd = new Plays { MusicianID = musician.ID, InstrumentID = int.Parse(inst) };
+                        musician.Plays.Add(instToAdd);
+                    }
+                }
+                
+
                 if (ModelState.IsValid)
                 {
                     _context.Add(musician);
@@ -82,6 +180,7 @@ namespace kdubois1_MVC_Music.Controllers
                 }
             }
 
+            PopulateAssignedInstrumentData(musician);
             PopulateInstrumentDropdown(musician);
 
             return View(musician);
@@ -95,14 +194,19 @@ namespace kdubois1_MVC_Music.Controllers
                 return NotFound();
             }
 
-            var musician = await _context.Musicians.FindAsync(id);
+            var musician = await _context.Musicians
+                .Include(m => m.Instrument)
+                .Include(m => m.Plays).ThenInclude(m => m.Instrument)
+                .AsNoTracking()
+                .SingleOrDefaultAsync(m => m.ID == id);
+
             if (musician == null)
             {
                 return NotFound();
             }
 
             PopulateInstrumentDropdown(musician);
-
+            PopulateAssignedInstrumentData(musician);
             return View(musician);
         }
 
@@ -111,14 +215,19 @@ namespace kdubois1_MVC_Music.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id)
+        public async Task<IActionResult> Edit(int id, string[] selectedInstruments)
         {
-             var musicianToUpdate = await _context.Musicians.SingleOrDefaultAsync(m => m.ID == id);
+             var musicianToUpdate = await _context.Musicians
+                .Include(m => m.Instrument)
+                .Include(m => m.Plays).ThenInclude(m => m.Instrument)
+                .SingleOrDefaultAsync(m => m.ID == id);
 
             if (musicianToUpdate == null)
             {
                 return NotFound();
             }
+
+            UpdatePlays(selectedInstruments, musicianToUpdate);
 
             if (await TryUpdateModelAsync<Musician>(musicianToUpdate, "",
                m => m.StageName,m => m.FName, m => m.MName, m => m.LName, m => m.Phone, m => m.DOB, m => m.SIN, m => m.InstrumentID))
@@ -152,6 +261,7 @@ namespace kdubois1_MVC_Music.Controllers
                 }
             }
             //Validaiton Error so give the user another chance.
+            PopulateAssignedInstrumentData(musicianToUpdate);
             PopulateInstrumentDropdown(musicianToUpdate);
             return View(musicianToUpdate);
         }
@@ -211,6 +321,53 @@ namespace kdubois1_MVC_Music.Controllers
             var iQuery = new SelectList(_context.Instruments
                 .OrderBy(i => i.Name), "ID", "Name", musician?.InstrumentID);
             ViewData["InstrumentID"] = iQuery;
+        }
+
+        private void PopulateAssignedInstrumentData(Musician musician)
+        {
+            var allInstruments = _context.Instruments;
+            var mInstruments = new HashSet<int>(musician.Plays.Select(i => i.InstrumentID));
+            var viewModel = new List<PlaysVM>();
+            foreach(var ins in allInstruments)
+            {
+                viewModel.Add(new PlaysVM
+                {
+                    InstrumentID = ins.ID,
+                    InstrumentName = ins.Name,
+                    Assigned = mInstruments.Contains(ins.ID)
+                });
+            }
+            ViewData["Instruments"] = viewModel;
+        }
+
+        private void UpdatePlays(string[] selectedInstruments, Musician musicianToUpdate)
+        {
+            if(selectedInstruments == null)
+            {
+                musicianToUpdate.Plays = new List<Plays>();
+                return;
+            }
+
+            var selectedInstrumentsHS = new HashSet<string>(selectedInstruments);
+            var playsHS = new HashSet<int>(musicianToUpdate.Plays.Select(i => i.InstrumentID));
+            foreach(var ins in _context.Instruments)
+            {
+                if (selectedInstrumentsHS.Contains(ins.ID.ToString()))
+                {
+                    if (!playsHS.Contains(ins.ID))
+                    {
+                        musicianToUpdate.Plays.Add(new Plays { MusicianID = musicianToUpdate.ID, InstrumentID = ins.ID });
+                    }
+                }
+                else
+                {
+                    if (playsHS.Contains(ins.ID))
+                    {
+                        Plays playsToRemove = musicianToUpdate.Plays.SingleOrDefault(i => i.InstrumentID == ins.ID);
+                        _context.Remove(playsToRemove);
+                    }
+                }
+            }
         }
 
         private bool MusicianExists(int id)
